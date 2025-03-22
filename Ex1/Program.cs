ba7c3e939b29b9002e3de7822349dc8d5ddda1f0
Ivan Iliev <iii@debian>
2025-03-22 10:53:41 +0200

    Added Program.cs

---
+++ Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Ex1
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			unsafe
+			{
+				int a = 5;
+				int *address = &a;
+				GCHandle handle = GCHandle.Alloc(a, GCHandleType.Pinned);
+    				IntPtr pointer = GCHandle.ToIntPtr(handle);
+
+				System.Console.WriteLine($"{*address}");
+				System.Console.WriteLine($"{handle.Target} => {pointer}");
+
+				handle.Free();
+			}
+			System.Console.WriteLine("Here!");
+		}
+	}
+}
